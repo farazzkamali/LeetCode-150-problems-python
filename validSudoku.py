23e38114b33a8527a695b4274597c8d6343676be
farazzkamali <farazzkamali80@gmail.com>
2023-08-28 20:43:58 +0330

    valid sudoku

---
+++ validSudoku.py
@@ -0,0 +1,54 @@
+class Solution:
+    def isValidSudoku(board):
+        # Initialize sets to keep track of seen digits in rows, columns, and sub-boxes
+        rows = [set() for _ in range(9)]
+        columns = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for i in range(9):
+            for j in range(9):
+                num = board[i][j]
+                if num != ".":
+                    # Check row
+                    if num in rows[i]:
+                        return False
+                    rows[i].add(num)
+
+                    # Check column
+                    if num in columns[j]:
+                        return False
+                    columns[j].add(num)
+
+                    # Check sub-box
+                    box_index = (i // 3) * 3 + j // 3
+                    if num in boxes[box_index]:
+                        return False
+                    boxes[box_index].add(num)
+
+        return True
+
+
+# Example usage
+board1 = [
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
+
+board2 = [
+    ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
