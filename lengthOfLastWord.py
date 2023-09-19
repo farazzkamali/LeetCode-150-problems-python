44e2cec2a8b6f80a7412afdf8c13f080a4cacd50
farazzkamali <farazzkamali80@gmail.com>
2023-09-19 18:15:54 +0330

    lenght of last word 2solutions

--- lengthOfLastWord.py
+++ lengthOfLastWord.py
@@ -14,3 +14,15 @@ print(solution.lengthOfLastWord("     banana     this    ")
       )                    # Output: 5
 print(solution.lengthOfLastWord("   fly me   to   the moon  "))    # Output: 4
 print(solution.lengthOfLastWord("luffy is still joyboy"))          # Output: 6
+
+
+# class Solution:
+#     def lengthOfLastWord(self, s: str) -> int:
+#         i = len(s) - 1
+#         lenght = 0
+#         while s[i] == " ":
+#             i -= 1
+#         while i >= 0 and s[i] != " ":
+#             lenght += 1
+#             i -= 1
+#         return lenght
