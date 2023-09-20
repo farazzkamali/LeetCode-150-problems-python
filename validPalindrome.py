9d7ccae454988cf3ad3fca9d2a9a912d87480772
farazzkamali <farazzkamali80@gmail.com>
2023-09-20 18:39:09 +0330

    vali palindrome 2solutions

--- validPalindrome.py
+++ validPalindrome.py
@@ -1,11 +1,28 @@
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         s_arr = []
+#         for word in s:
+#             if word.isalpha() or word.isalnum():
+#                 s_arr.append(word.lower())
+#         s_arr_reverse = s_arr[::-1]
+#         if s_arr == s_arr_reverse:
+#             return True
+#         else:
+#             return False
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s_arr = []
-        for word in s:
-            if word.isalpha() or word.isalnum():
-                s_arr.append(word.lower())
-        s_arr_reverse = s_arr[::-1]
-        if s_arr == s_arr_reverse:
-            return True
-        else:
-            return False
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            while l < r and not self.isAlpha(s[l]):
+                l += 1
+            while r < l and not self.isAlpha(s[r]):
+                r += 1
+            if s[l].lower() != s[r].lower():
+                return False
+        return True
+
+    def isAlpha(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
