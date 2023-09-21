89e2f1d1c925d32520227b9e49dc1396228cda5b
farazzkamali <farazzkamali80@gmail.com>
2023-09-21 18:07:28 +0330

    contains duplicate

---
+++ containsDuplicate.py
@@ -0,0 +1,6 @@
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        unique = set(nums)
+        if len(nums) > len(unique):
+            return True
+        return False
