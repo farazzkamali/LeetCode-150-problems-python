866de663791cd75f62f795f5afff3d4f55f25c27
farazzkamali <farazzkamali80@gmail.com>
2023-09-25 17:42:51 +0330

    squares of sorted array wsolutions

--- squaresOfSortedArry.py
+++ squaresOfSortedArry.py
@@ -1,6 +1,22 @@
+# class Solution:
+#     def sortedSquares(self, nums: list[int]) -> list[int]:
+#         for i in range(0, len(nums)):
+#             nums[i] = nums[i] ** 2
+#         nums.sort()
+#         return nums
+
+
 class Solution:
-    def sortedSquares(self, nums: list[int]) -> list[int]:
-        for i in range(0, len(nums)):
-            nums[i] = nums[i] ** 2
-        nums.sort()
-        return nums
+    def sortedSquares(self, nums):
+        left = 0
+        right = len(nums)-1
+        res = []
+
+        while left <= right:
+            if nums[left] * nums[left] > nums[right] * nums[right]:
+                res.append(nums[left] * nums[left])
+                left += 1
+            else:
+                res.append(nums[right] * nums[right])
+                right -= 1
+        return res[::-1]
